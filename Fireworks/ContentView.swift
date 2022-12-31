8676cc317432b4418efb948972aafeed79228d5d
LiYanan2004 <37542129+LiYanan2004@users.noreply.github.com>
2022-12-31 10:26:49 +0800

    Initial Commit

---
+++ Fireworks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fireworks
+//
+//  Created by LiYanan2004 on 2022/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fireworks/FireworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireworksApp.swift
+//  Fireworks
+//
+//  Created by LiYanan2004 on 2022/12/31.
+//
+
+import SwiftUI
+
+@main
+struct FireworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
